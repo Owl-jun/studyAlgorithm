aa280478ce167a197d4a2762c67521b47f10c21c
owl-jun <owljun2@gmail.com>
2021-07-21 17:48:30 +0900

    programmers.co.kr

--- hash.py
+++ hash.py
@@ -47,3 +47,30 @@ class ChainedHash:
         if isinstance(self, key):
             return key % self.capacity
         return(int(hashlib.sha256(str(key).encode()).hexdigest(),16) % self.capacity)
+
+    def search(self, key: Any) -> Any:
+        """키가 key인 원소를 검색하여 값을 반환"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+        
+        while p is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+
+        return None
+
+    def add(self, key: Any, value: Any) -> bool:
+        """키가 key이고 값이 value인 원소를 추가"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return False
+            p = p.next
+
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp
+        return True
+        
---
+++ programers.py
@@ -0,0 +1,25 @@
+#
+
+n = 3
+text = ['abc','cde','exercize','expo','origin','normal','lack','korea','abd']
+
+def solution(n,text):
+    bucket = []
+    cnt = 0
+    num = 0
+    for i in text:
+        if i in bucket:
+            if len(text) % n == 0:
+                num = n
+            else:
+                num = len(text) % n  
+        bucket.append(i)
+        if len(bucket)%n == 1:
+            cnt += 1
+    if num == 0:
+        cnt = 0
+        num = 0
+
+    return [cnt,num]
+    
+print(solution(n,text))
