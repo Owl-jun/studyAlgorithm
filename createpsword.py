86b3c19aba171703e5c608fbf63e5667745ad5eb
owl-jun <owljun2@gmail.com>
2021-07-30 13:20:26 +0900

    createpsword

--- createpsword.py
+++ createpsword.py
@@ -10,3 +10,12 @@ def createpsword(passlen):
     return password
 
 print(f'추천 비밀번호 : {createpsword(8)}')
+
+
+while True:
+    i1 = int(input('마음에 안드시나요? (다시뽑으려면 0, 종료하려면 1 입력) :'))
+    if i1 == 0:
+        print(f'추천 비밀번호 : {createpsword(8)}')
+    else:
+        print('프로그램을 종료합니다.')
+        break
